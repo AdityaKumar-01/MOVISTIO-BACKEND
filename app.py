6a2384392fe91d7165635702929226c90e3b1adf
aditya <aditya7238195@gmail.com>
2021-11-03 13:00:12 +0530

    :hammer: files modified for deployment

--- app.py
+++ app.py
@@ -105,5 +105,5 @@ def filterReviews():
 
     return res
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True,port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False,host="0.0.0.0", port=port)
